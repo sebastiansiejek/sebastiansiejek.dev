f27a54921c1e65a94d78cc4800bc4d9c279933c4
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-01-29 21:21:26 +0100

    feat: add socials and name on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Socials from 'components/containers/Socials'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <h1>Homepage</h1>
+  return (
+    <div className="bg-n w-screen h-screen flex flex-col justify-center items-center text-center">
+      <h1 className="text-primary text-5xl md:text-7xl uppercase font-mono">
+        Sebastian Siejek
+      </h1>
+      <Socials />
+    </div>
+  )
 }
 
 export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      mono: "'Inconsolata', monospace",
+    },
+    colors: {
+      primary: {
+        DEFAULT: '#3CEAB8',
+      },
+      n: {
+        DEFAULT: '#17181D',
+      },
+    },
   },
   plugins: [],
 }
