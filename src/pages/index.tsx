3f4985ed6e280d3aa0a23be6e96b9a2ace9b49c2
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-02-10 22:28:49 +0100

    feat: add seo tags to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,30 @@
 import Socials from 'components/containers/Socials'
 import type { NextPage } from 'next'
+import { NextSeo } from 'next-seo'
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-n w-screen h-screen flex flex-col justify-center items-center text-center">
-      <h1 className="text-primary text-5xl md:text-7xl uppercase font-mono">
-        Sebastian Siejek
-      </h1>
-      <Socials />
-    </div>
+    <>
+      <NextSeo
+        title="Sebastian Siejek"
+        description="Sebastian Siejek - Web Developer"
+        canonical="https://sebastiansiejek.dev"
+        additionalMetaTags={[
+          { name: 'author', content: 'Sebastian Siejek' },
+          {
+            name: 'keywords',
+            content:
+              'web, developer, software, engineer, javascript, react, nextjs, php, wordpress, empressia',
+          },
+        ]}
+      />
+      <div className="bg-n w-screen h-screen flex flex-col justify-center items-center text-center">
+        <h1 className="text-primary text-5xl md:text-7xl uppercase font-mono">
+          Sebastian Siejek
+        </h1>
+        <Socials />
+      </div>
+    </>
   )
 }
 
