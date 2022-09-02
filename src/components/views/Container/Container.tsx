1f928a05aed5a11cb4f4e6a35ab827eb5df30cdf
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-09-02 18:28:24 +0200

    feat: set container max width on 1168px

--- src/components/views/Container/Container.tsx
+++ src/components/views/Container/Container.tsx
@@ -3,9 +3,7 @@ import { FC, ReactNode } from 'react'
 const Container: FC<{
   children: ReactNode
 }> = ({ children }) => {
-  return (
-    <div className={'max-w-screen-xl mx-auto px-8 xl:px-0'}>{children}</div>
-  )
+  return <div className={'max-w-[1168px] mx-auto px-8 xl:px-0'}>{children}</div>
 }
 
 export default Container
