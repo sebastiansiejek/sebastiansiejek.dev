8f3ce1baaf33641485518531ad25259b3d0a3cfb
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-09-02 18:32:29 +0200

    feat: remove upperace and increase letter spacing of SiteBranding

--- src/components/views/SiteBranding/SiteBranding.tsx
+++ src/components/views/SiteBranding/SiteBranding.tsx
@@ -2,7 +2,7 @@ const SiteBranding = () => {
   return (
     <span
       aria-label={'Site branding'}
-      className={'uppercase text-primary font-mono text-3xl tracking-widest'}
+      className={'text-primary font-mono text-3xl tracking-widest'}
     >
       SebastianSiejek.dev
     </span>
