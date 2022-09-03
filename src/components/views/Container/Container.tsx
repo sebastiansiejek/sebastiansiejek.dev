7a2a79923812243a7870195cc3e5469951769947
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-09-03 12:35:29 +0200

    feat: add custom className to Container.tsx

--- src/components/views/Container/Container.tsx
+++ src/components/views/Container/Container.tsx
@@ -1,9 +1,19 @@
 import { FC, ReactNode } from 'react'
+import clsx from 'clsx'
 
 const Container: FC<{
   children: ReactNode
-}> = ({ children }) => {
-  return <div className={'max-w-[1168px] mx-auto px-8 xl:px-0'}>{children}</div>
+  className?: string
+}> = ({ children, className }) => {
+  return (
+    <div
+      className={clsx('max-w-[1168px] mx-auto px-8 xl:px-0 w-full', {
+        [className || '']: className,
+      })}
+    >
+      {children}
+    </div>
+  )
 }
 
 export default Container
