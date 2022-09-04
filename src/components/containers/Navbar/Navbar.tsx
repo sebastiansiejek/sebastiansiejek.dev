b85a28721dba206e6273ff478f44b46214781485
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-09-04 21:16:09 +0200

    feat: wrap SiteBranding in navbar in link to homepage

--- src/components/containers/Navbar/Navbar.tsx
+++ src/components/containers/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import SiteBranding from '../../views/SiteBranding'
 import Container from '../../views/Container'
 import NavbarMenu from './NavbarMenu'
 import NavbarMenuMobile from './NavbarMenuMobile'
+import Link from 'next/link'
 
 const menu = [
   {
@@ -19,7 +20,11 @@ const Navbar = () => {
     <nav aria-label={'Navigation'} className={'bg-n py-2 text-n-0 text-xl'}>
       <Container>
         <div className={'flex items-center justify-between'}>
-          <SiteBranding />
+          <Link href={'/'} passHref>
+            <a>
+              <SiteBranding />
+            </a>
+          </Link>
           <div className="ml-10 hidden lg:block">
             <NavbarMenu data={menu} />
           </div>
