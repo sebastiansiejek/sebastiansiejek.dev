136f2ed485670203c8600491ad25cc8ccb49bf26
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-09-04 13:25:53 +0200

    feat: decrease font size of SiteBranding

--- src/components/views/SiteBranding/SiteBranding.tsx
+++ src/components/views/SiteBranding/SiteBranding.tsx
@@ -2,7 +2,7 @@ const SiteBranding = () => {
   return (
     <span
       aria-label={'Site branding'}
-      className={'text-primary font-mono text-3xl tracking-widest'}
+      className={'text-primary font-mono text-xl md:text-3xl tracking-widest'}
     >
       SebastianSiejek.dev
     </span>
