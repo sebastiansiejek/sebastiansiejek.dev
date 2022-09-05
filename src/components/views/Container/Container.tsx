929a5dbcea4d884e3935610b7e37d81186e28df3
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-09-05 18:12:22 +0200

    feat: add tight size to container

--- src/components/views/Container/Container.tsx
+++ src/components/views/Container/Container.tsx
@@ -4,11 +4,14 @@ import clsx from 'clsx'
 const Container: FC<{
   children: ReactNode
   className?: string
-}> = ({ children, className }) => {
+  size?: 'tight' | 'medium'
+}> = ({ children, className, size = 'medium' }) => {
   return (
     <div
-      className={clsx('max-w-[1168px] mx-auto px-8 xl:px-0 w-full', {
+      className={clsx('mx-auto px-8 xl:px-0 w-full', {
         [className || '']: className,
+        'max-w-[1168px]': size === 'medium',
+        'max-w-screen-sm': size === 'tight',
       })}
     >
       {children}
