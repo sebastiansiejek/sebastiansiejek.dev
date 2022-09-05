6b65e3b711925b7b56a9775f8077add4085b2b4c
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-09-05 21:55:51 +0200

    feat: add footer

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -3,6 +3,7 @@ import { NextSeo } from 'next-seo'
 import { getAllResources } from '../../lib/resourcesService'
 import path from 'path'
 import BlogPosts from '../../components/views/BlogPosts'
+import Socials from '../../components/containers/Socials'
 
 interface IBlogProps {
   posts: IPosts
@@ -13,9 +14,17 @@ const Blog = (props: IBlogProps) => {
     <>
       <NextSeo title={'Blog - SebastianSiejek.dev'} />
       <Navbar />
-      <main className="py-12 min-h-[100vw]">
+      <main className="py-12 min-h-[100vh]">
         <BlogPosts posts={props.posts} />
       </main>
+      <footer className={'bg-n py-12 text-n-0'}>
+        <div className="flex items-center justify-center flex-col">
+          <Socials />
+          <p className={'mt-5'}>
+            © 2022 sebastiansiejek.dev. All rights reserved
+          </p>
+        </div>
+      </footer>
     </>
   )
 }
