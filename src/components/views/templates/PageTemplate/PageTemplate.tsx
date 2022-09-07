17dd852a08bc0035ee599446247e66940805df49
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-09-07 17:10:26 +0200

    feat: add PageTemplate and apply to Blog

---
+++ src/components/views/templates/PageTemplate/PageTemplate.tsx
@@ -0,0 +1,26 @@
+import Socials from '../../../containers/Socials'
+import Navbar from '../../../containers/Navbar'
+import { ReactNode } from 'react'
+
+interface IPageTemplate {
+  children: ReactNode
+}
+
+const PageTemplate = ({ children }: IPageTemplate) => {
+  return (
+    <>
+      <Navbar />
+      <main className="py-12">{children}</main>
+      <footer className={'bg-n py-12 text-n-0'}>
+        <div className="flex items-center justify-center flex-col">
+          <Socials />
+          <p className={'mt-5'}>
+            © 2022 sebastiansiejek.dev. All rights reserved
+          </p>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default PageTemplate
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,8 @@
-import Navbar from 'components/containers/Navbar'
 import { NextSeo } from 'next-seo'
 import { getAllResources } from '../../lib/resourcesService'
 import path from 'path'
 import BlogPosts from '../../components/views/BlogPosts'
-import Socials from '../../components/containers/Socials'
+import PageTemplate from '../../components/views/templates/PageTemplate'
 
 interface IBlogProps {
   posts: IPosts
@@ -13,18 +12,9 @@ const Blog = (props: IBlogProps) => {
   return (
     <>
       <NextSeo title={'Blog - SebastianSiejek.dev'} />
-      <Navbar />
-      <main className="py-12 min-h-[100vh]">
+      <PageTemplate>
         <BlogPosts posts={props.posts} />
-      </main>
-      <footer className={'bg-n py-12 text-n-0'}>
-        <div className="flex items-center justify-center flex-col">
-          <Socials />
-          <p className={'mt-5'}>
-            © 2022 sebastiansiejek.dev. All rights reserved
-          </p>
-        </div>
-      </footer>
+      </PageTemplate>
     </>
   )
 }
