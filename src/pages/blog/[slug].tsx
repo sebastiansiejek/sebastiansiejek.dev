c0b77a7a1b9aa9be772d4dacb856ad3f9b3af791
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-09-18 21:12:12 +0200

    feat: display single blog post

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,13 +1,15 @@
 import {
   getResourceBySlug,
   getResourcesPaths,
-} from '../../lib/resourcesService'
+} from '../../lib/resources/resourcesService'
 import path from 'path'
 import PageTemplate from '../../components/views/templates/PageTemplate'
 import Container from '../../components/views/Container'
 import { GetStaticProps } from 'next'
 import { MDXRemote } from 'next-mdx-remote'
 import { NextSeo } from 'next-seo'
+import 'dracula-prism/dist/css/dracula-prism.min.css'
+import ContentStyled from '../../styles/ContentStyled'
 
 const SinglePost = ({ post }: any) => {
   return (
@@ -15,17 +17,27 @@ const SinglePost = ({ post }: any) => {
       <NextSeo title={`${post.slug} - sebastiansiejek.dev`} />
       <PageTemplate>
         <Container size={'tight'}>
-          <MDXRemote
-            {...post.transformedMdx}
-            components={{
-              code: (props) => (
-                <code
-                  className={'text-primary bg-n font-mono p-1'}
-                  {...props}
-                />
-              ),
-            }}
-          />
+          <ContentStyled>
+            <MDXRemote
+              {...post.transformedMdx}
+              components={{
+                code: (props) => (
+                  <code
+                    className={'text-primary bg-n font-mono p-1'}
+                    {...props}
+                  />
+                ),
+                ol: (list) => {
+                  return (
+                    <ol className={'list-decimal pl-4'}>{list.children}</ol>
+                  )
+                },
+                ul: (list) => {
+                  return <ul className={'list-disc pl-4'}>{list.children}</ul>
+                },
+              }}
+            />
+          </ContentStyled>
         </Container>
       </PageTemplate>
     </>
---
+++ src/styles/ContentStyled.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const ContentStyled = styled.div`
+  > {
+    *,
+    pre {
+      line-height: 1.8rem;
+      &:not(:first-child) {
+        margin-top: 3.2rem;
+      }
+    }
+  }
+`
+
+export default ContentStyled
