70e9588511fccea4abd62815bcd8dc4ef153a44a
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-10-02 12:08:29 +0200

    feat: use useBrowserTheme hook in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import Favicon from 'components/Favicon'
 import type { AppProps } from 'next/app'
 import 'styles/global.css'
+import useBrowserTheme from '../hooks/useBrowserTheme'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useBrowserTheme()
+
   return (
     <>
       <Favicon />
