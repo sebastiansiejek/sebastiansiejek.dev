bed3368758fc6d19aed738ecf6ed0f668453782f
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-10-02 14:32:03 +0200

    chore: set @svgr/webpack

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: {
+        loader: '@svgr/webpack',
+        options: {
+          svgoConfig: {
+            plugins: [
+              {
+                name: 'removeViewBox',
+                active: false,
+              },
+            ],
+          },
+        },
+      },
+    })
+
+    return config
+  },
 }
 
 module.exports = nextConfig
