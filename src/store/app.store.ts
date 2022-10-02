dd20567cf3fdc1a09a5e4d5dae0e330f42ccc48f
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-10-02 12:07:59 +0200

    feat: add App Store with toggle theme

---
+++ src/store/app.store.ts
@@ -0,0 +1,26 @@
+import create from 'zustand'
+import { devtools, persist } from 'zustand/middleware'
+
+interface AppStore {
+  theme: 'light' | 'dark' | ''
+  toggleTheme: (by: AppStore['theme']) => void
+}
+
+const useAppStore = create<AppStore>()(
+  devtools(
+    persist(
+      (set) => ({
+        theme: '',
+        toggleTheme: (by) => {
+          document.documentElement.classList.toggle('dark')
+          return set((state) => ({ theme: by }))
+        },
+      }),
+      {
+        name: 'app-store',
+      },
+    ),
+  ),
+)
+
+export default useAppStore
