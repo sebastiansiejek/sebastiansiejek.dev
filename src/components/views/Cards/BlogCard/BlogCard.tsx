a0998741c18399a18cfda10ee223879c10c994a4
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-10-03 22:11:53 +0200

    fix: set white background of blog card

--- src/components/views/Cards/BlogCard/BlogCard.tsx
+++ src/components/views/Cards/BlogCard/BlogCard.tsx
@@ -33,7 +33,7 @@ const BlogCard = ({ slug, thumbnail, tags, title, excerpt }: IPost) => {
                 return (
                   <span
                     key={tag}
-                    className="inline-block bg-n-1 dark:bg-n dark:border-n-0 border rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                    className="inline-block bg-n-0 dark:bg-n dark:border-n-0 border rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
                   >
                     #{tag}
                   </span>
