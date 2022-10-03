7cd06db6af54a46989fbeb3e5e73b1009c671d85
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-10-03 22:10:41 +0200

    feat: disable ThemeModeSwitcher on homepage

--- src/components/containers/ThemeModeSwitcher/ThemeModeSwitcher.tsx
+++ src/components/containers/ThemeModeSwitcher/ThemeModeSwitcher.tsx
@@ -1,12 +1,16 @@
 import useAppStore from '../../../store/app.store'
 import DarkModeIcon from '../../../../public/images/icons/dark-mode.svg'
 import LightModeIcon from '../../../../public/images/icons/light-mode.svg'
+import { useRouter } from 'next/router'
 
 const ThemeModeSwitcher = () => {
   const toggleTheme = useAppStore((state) => state.toggleTheme)
   const theme = useAppStore((state) => state.theme)
   const isDarkMode = theme === 'dark'
   const iconClasses = 'w-6 h-6 fill-n-0'
+  const router = useRouter()
+
+  if (router.pathname === '/') return null
 
   return (
     <button onClick={() => toggleTheme(isDarkMode ? 'light' : 'dark')}>
