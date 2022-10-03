2c39c26f1a7e6c3d2516d1ceb27398e7f53e003d
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-10-03 22:16:11 +0200

    feat: revert content of 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,22 @@
-import FullPageTemplate from 'components/views/templates/FullPageTemplate'
 import { NextSeo } from 'next-seo'
+import Navbar from '../components/containers/Navbar'
+import Socials from '../components/containers/Socials'
 
 export default function Custom404() {
   return (
     <>
       <NextSeo title="404" nofollow noindex />
-      <FullPageTemplate title="404" />
+      <div>
+        <div className={'fixed w-full z-10'}>
+          <Navbar />
+        </div>
+        <div className="bg-n w-screen h-screen flex flex-col justify-center items-center text-center">
+          <h1 className="text-primary text-5xl md:text-7xl uppercase font-mono">
+            404
+          </h1>
+          <Socials />
+        </div>
+      </div>
     </>
   )
 }
