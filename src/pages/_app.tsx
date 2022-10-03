e1eb45655cf3e189f5d0809854a0118e6db6ee40
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-10-03 21:24:37 +0200

    feat: implement NextNProgress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import Favicon from 'components/Favicon'
 import type { AppProps } from 'next/app'
 import 'styles/global.css'
 import useBrowserTheme from '../hooks/useBrowserTheme'
+import NextNProgress from 'nextjs-progressbar'
+import tailwind from '../../tailwind.config'
 
 function MyApp({ Component, pageProps }: AppProps) {
   useBrowserTheme()
 
   return (
     <>
+      <NextNProgress color={tailwind.theme.colors.primary.DEFAULT} />
       <Favicon />
       <Component {...pageProps} />
     </>
