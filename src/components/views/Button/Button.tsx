9a79cd9dd4e7626fea77251efbe9d08091172679
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-10-03 21:56:40 +0200

    feat: add ButtonLink variant

--- src/components/views/Button/Button.tsx
+++ src/components/views/Button/Button.tsx
@@ -1,15 +1,17 @@
 import { ButtonHTMLAttributes } from 'react'
 import ButtonOutline from './ButtonOutline'
+import ButtonLink from './ButtonLink'
 
 export interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
   isActive?: boolean
-  variant?: 'outline'
+  variant?: 'outline' | 'link'
   isFullWidth?: boolean
   isLoading?: boolean
 }
 
 const Button = ({ variant, ...props }: IButton) => {
   if (variant === 'outline') return <ButtonOutline {...props} />
+  if (variant === 'link') return <ButtonLink {...props} />
 
   return null
 }
