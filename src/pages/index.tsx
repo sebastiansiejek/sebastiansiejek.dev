ecf06a6185d8016ee33e21b25f5c1347498261a6
Sebastian Siejek <contact@sebastiansiejek.dev>
2022-10-15 16:24:58 +0200

    feat: add name nad job title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,6 +24,14 @@ const Home: NextPage = () => {
           <Navbar />
         </div>
         <div className="bg-n w-screen h-screen flex flex-col justify-center items-center text-center">
+          <h1
+            className={'text-3xl md:text-5xl font-mono text-primary uppercase'}
+          >
+            Sebastian Siejek
+          </h1>
+          <h2 className={'text-xl md:text-3xl font-mono text-primary mb-6'}>
+            Software Engineer
+          </h2>
           <Socials />
         </div>
       </div>
