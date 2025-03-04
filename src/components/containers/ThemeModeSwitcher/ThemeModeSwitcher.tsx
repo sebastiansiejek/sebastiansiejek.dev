e91cd1b7aac9bb1260f5afdd4357baee46b9fcc4
Sebastian Siejek <siejeksebastian@gmail.com>
2025-03-04 20:00:34 +0100

    refactor: replace theme with resolvedTheme in ThemeModeSwitcher
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

--- src/components/containers/ThemeModeSwitcher/ThemeModeSwitcher.tsx
+++ src/components/containers/ThemeModeSwitcher/ThemeModeSwitcher.tsx
@@ -4,10 +4,9 @@ import { useTheme } from 'next-themes'
 
 const ThemeModeSwitcher = () => {
   const iconClasses = 'w-4 h-4 fill-n-0'
-  const { theme, setTheme } = useTheme()
-  const isDarkMode = theme === 'dark'
+  const { setTheme, resolvedTheme } = useTheme()
+  const isDarkMode = resolvedTheme === 'dark'
   const router = useRouter()
-  console.log(theme)
 
   if (router.pathname === '/') return null
 
