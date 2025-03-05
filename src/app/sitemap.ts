455908122b1ced41e1b297a4b81341a9344491e7
Sebastian Siejek <siejeksebastian@gmail.com>
2025-03-05 17:14:10 +0100

    feat(sitemap): replace `next-sitemap` with custom sitemap and robots implementations
    - Removed `next-sitemap` dependency and its configuration.
    - Added custom `sitemap.ts` and `robots.ts` to generate sitemap and robots.txt logic.
    - Enhanced sitemap generation with blog post URLs and metadata.
    - Added backlinks to blog posts for improved SEO.
    - Adjusted package dependencies to reflect removal of `next-sitemap`.
    
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

---
+++ src/app/sitemap.ts
@@ -0,0 +1,31 @@
+import { MetadataRoute } from 'next'
+import * as process from 'node:process'
+import { getAllResources } from 'lib/resources/resourcesService'
+import path from 'path'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const blogPosts: MetadataRoute.Sitemap = getAllResources(
+    path.join(process.cwd(), 'src/content/posts'),
+  ).map((post) => ({
+    url: `${process.env.SITE_URL}/blog/${post.slug}`,
+    lastModified: new Date(post.publishedAt),
+    changeFrequency: 'monthly',
+    priority: 0.6,
+  }))
+
+  return [
+    {
+      url: process.env.SITE_URL,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${process.env.SITE_URL}/blog`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    ...blogPosts,
+  ]
+}
