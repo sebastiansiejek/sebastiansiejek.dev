7736544eb16ba3817f3ecc2d7d1724ba8e0bd7b3
Sebastian Siejek <siejeksebastian@gmail.com>
2025-03-09 19:53:17 +0100

    feat: add useIsMounted hook and integrate in ThemeModeSwitcher
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

--- src/widgets/ThemeModeSwitcher/ThemeModeSwitcher.tsx
+++ src/widgets/ThemeModeSwitcher/ThemeModeSwitcher.tsx
@@ -3,12 +3,16 @@
 import { CiDark, CiLight } from 'react-icons/ci'
 import { usePathname } from 'next/navigation'
 import { useTheme } from 'next-themes'
+import { useIsMounted } from 'shared/hooks/useIsMounted/useIsMounted'
 
 const ThemeModeSwitcher = () => {
   const { setTheme, resolvedTheme } = useTheme()
   const pathname = usePathname()
+  const mounted = useIsMounted()
 
   if (pathname === '/') return null
+
+  if (!mounted) return null
 
   const iconClasses = 'w-4 h-4 fill-n-0'
   const isDarkTheme = resolvedTheme === 'dark'
