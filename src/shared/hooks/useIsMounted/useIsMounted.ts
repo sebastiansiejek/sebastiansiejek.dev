550b9e3e2f90622bc709b2699369a95551c9bf61
Sebastian Siejek <siejeksebastian@gmail.com>
2025-05-18 14:23:06 +0200

    chore: change reacti-icons with lucide-react
    Signed-off-by: Sebastian Siejek <siejeksebastian@gmail.com>

--- src/shared/hooks/useIsMounted/useIsMounted.ts
+++ src/shared/hooks/useIsMounted/useIsMounted.ts
@@ -2,11 +2,10 @@ import { useEffect, useState } from 'react'
 
 export const useIsMounted = () => {
   const [isMounted, setIsMounted] = useState(false)
+
   useEffect(() => {
     setIsMounted(true)
   }, [])
 
-  if (!isMounted) {
-    return null
-  }
+  return isMounted
 }
--- src/widgets/ThemeModeSwitcher/ThemeModeSwitcher.tsx
+++ src/widgets/ThemeModeSwitcher/ThemeModeSwitcher.tsx
@@ -1,28 +1,23 @@
 'use client'
 
-import { CiDark, CiLight } from 'react-icons/ci'
-import { usePathname } from 'next/navigation'
+import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 import { useIsMounted } from '@/shared/hooks/useIsMounted/useIsMounted'
 
 const ThemeModeSwitcher = () => {
   const { setTheme, resolvedTheme } = useTheme()
-  const pathname = usePathname()
-  const mounted = useIsMounted()
-
-  const iconClasses = 'w-4 h-4'
   const isDarkTheme = resolvedTheme === 'dark'
+  const isMounted = useIsMounted()
+
+  if (!isMounted) return null
 
   return (
     <button
       onClick={() => setTheme(isDarkTheme ? 'light' : 'dark')}
       className={'transition cursor-pointer hover:opacity-80'}
     >
-      {isDarkTheme ? (
-        <CiLight className={iconClasses} />
-      ) : (
-        <CiDark className={iconClasses} />
-      )}
+      {isDarkTheme && <Sun className={'w-4 h-4'} />}
+      {!isDarkTheme && <Moon className={'w-4 h-4'} />}
     </button>
   )
 }
